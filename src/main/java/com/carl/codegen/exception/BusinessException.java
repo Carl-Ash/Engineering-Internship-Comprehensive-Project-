b79d900950835b4ae52feebbb728dd3c5d47c0ee
Carl <1664687795@qq.com>
2026-04-14 20:32:34 +0800

    后端初始化

---
+++ src/main/java/com/carl/codegen/exception/BusinessException.java
@@ -0,0 +1,26 @@
+package com.carl.codegen.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    /**
+     * 状态码
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+}
---
+++ src/test/java/com/carl/codegen/CodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carl.codegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
